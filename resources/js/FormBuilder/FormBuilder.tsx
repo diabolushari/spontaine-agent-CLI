9e0ad4ee9544d6fe6d3d2570f6a727caf8d796b2
Silpa <mail2silpaps@gmail.com>
2024-09-25 10:24:47 +0530

    login

--- resources/js/FormBuilder/FormBuilder.tsx
+++ resources/js/FormBuilder/FormBuilder.tsx
@@ -12,7 +12,6 @@ import DynamicSelectList from '@/ui/form/DynamicSelectList'
 import Button from '@/ui/button/Button'
 import ComboBox from '@/ui/form/ComboBox'
 
-
 export interface FormItem<
   T,
   K extends keyof L,
@@ -37,6 +36,7 @@ export interface FormItem<
     | 'number'
   hidden?: boolean
   disabled?: boolean
+  placeholder?: string
   list?: L[]
   displayKey?: G
   displayKey2?: G
@@ -81,7 +81,7 @@ export default function FormBuilder<
   errors,
   buttonText = 'Submit',
   buttonAlignment,
-  children
+  children,
 }: Props<T, U, K, G, L>) {
   const formStyle = cn('grid w-full grid-cols-1 md:grid-cols-2 gap-5', formStyles)
 
@@ -113,6 +113,7 @@ export default function FormBuilder<
               setValue={formItems[keyValue].setValue as (value: string) => unknown}
               error={errors != null ? errors[keyValue] : undefined}
               disabled={formItems[keyValue].disabled}
+              placeholder={formItems[keyValue].placeholder}
             />
           )}
           {formItems[keyValue].type === 'number' && !formItems[keyValue].hidden && (
@@ -123,6 +124,7 @@ export default function FormBuilder<
               type='number'
               error={errors != null ? errors[keyValue] : undefined}
               disabled={formItems[keyValue].disabled}
+              placeholder={formItems[keyValue].placeholder}
             />
           )}
           {formItems[keyValue].type === 'email' && !formItems[keyValue].hidden && (
@@ -133,6 +135,7 @@ export default function FormBuilder<
               setValue={formItems[keyValue].setValue as (value: string) => unknown}
               error={errors != null ? errors[keyValue] : undefined}
               disabled={formItems[keyValue].disabled}
+              placeholder={formItems[keyValue].placeholder}
             />
           )}
           {formItems[keyValue].type === 'password' && !formItems[keyValue].hidden && (
@@ -143,6 +146,7 @@ export default function FormBuilder<
               setValue={formItems[keyValue].setValue as (value: string) => unknown}
               error={errors != null ? errors[keyValue] : undefined}
               disabled={formItems[keyValue].disabled}
+              placeholder={formItems[keyValue].placeholder}
             />
           )}
           {formItems[keyValue].type === 'checkbox' && !formItems[keyValue].hidden && (
@@ -161,6 +165,7 @@ export default function FormBuilder<
               label={formItems[keyValue].label}
               error={errors != null ? errors[keyValue] : undefined}
               disabled={formItems[keyValue].disabled}
+              placeholder={formItems[keyValue].placeholder}
             />
           )}
           {formItems[keyValue].type === 'date' && !formItems[keyValue].hidden && (
@@ -244,7 +249,7 @@ export default function FormBuilder<
         </div>
       ))}
       {children}
-      <div className={cn('flex gap-5 col-start-1 ', buttonStyle)}>
+      <div className={cn('col-start-1 flex gap-5', buttonStyle)}>
         <FullSpinnerWrapper processing={loading}>
           <Button label={buttonText} />
         </FullSpinnerWrapper>
