bb00e6b01d3201533c58b936612690328b376123
Swaraj Krishnan <swarajkrishnan@outlook.com>
2024-10-29 13:39:38 +0530

    subset fixes

--- app/Http/Controllers/Subset/SubsetDropdownApiController.php
+++ app/Http/Controllers/Subset/SubsetDropdownApiController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Subset;
 
 use App\Http\Controllers\Controller;
+use App\Models\DataDetail\DataDetail;
+use App\Services\DataTable\QueryDataTable;
 use App\Services\DistributionHierarchy\DistributionHierarchy;
+use App\Services\TableNames;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 
 class SubsetDropdownApiController extends Controller implements HasMiddleware
 {
@@ -18,9 +22,39 @@ class SubsetDropdownApiController extends Controller implements HasMiddleware
         ];
     }
 
-    public function __invoke(Request $request, DistributionHierarchy $findDistributionLevel)
+    public function __invoke(Request $request, DistributionHierarchy $findDistributionLevel, QueryDataTable $queryDataTable): JsonResponse
     {
+        $levelInfo = $findDistributionLevel->findAllSection(Auth::user()->office_code);
 
-        return  $findDistributionLevel->findAllSection(Auth::user()->office_code);
+        if ($levelInfo == null) {
+            return response()
+                ->json([
+                    'level' => null,
+                    'record' => null,
+                ]);
+        }
+
+        $dataDetail = DataDetail::where('name', TableNames::DISTRIBUTION_HIERARCHY)
+            ->with('dateFields', 'dimensionFields.structure', 'measureFields', 'subjectArea')
+            ->first();
+
+        if ($dataDetail === null || ! Schema::hasTable($dataDetail->table_name)) {
+            return response()
+                ->json([
+                    'level' => null,
+                    'record' => null,
+                ]);
+        }
+
+        $record = $queryDataTable->query($dataDetail)
+            ->where($levelInfo['levelField'], Auth::user()->office_code)
+            ->first();
+
+        return response()
+            ->json([
+                'level' => $levelInfo['level'],
+                'record' => $record,
+            ]);
+
     }
 }
