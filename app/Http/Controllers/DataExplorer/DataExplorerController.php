6264fdb57a088779b4c7bba103fe1270559d5a68
Swaraj Krishnan <swarajkrishnan@outlook.com>
2024-11-25 02:02:59 +0530

    data explorer page

---
+++ app/Http/Controllers/DataExplorer/DataExplorerController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\DataExplorer;
+
+use App\Http\Controllers\Controller;
+use App\Models\Subset\SubsetDetail;
+use App\Models\SubsetGroup\SubsetGroup;
+use App\Models\SubsetGroup\SubsetGroupItem;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class DataExplorerController extends Controller implements HasMiddleware
+{
+    /**
+     * @return string[]
+     */
+    public static function middleware(): array
+    {
+        return [
+            'auth',
+        ];
+    }
+
+    public function __invoke(string $subsetGroup, Request $request): Response
+    {
+
+        $subsetGroup = SubsetGroup::where('name', $subsetGroup)->firstOrFail();
+
+        $subsetDetails = SubsetGroupItem::where('subset_group_id', $subsetGroup->id)
+            ->pluck('subset_detail_id')
+            ->toArray();
+
+        $subsets = SubsetDetail::whereIn('id', $subsetDetails)
+            ->with('dimensions.info', 'measures.info', 'measures.weightInfo', 'dates.info')
+            ->get();
+
+        return Inertia::render('DataExplorer/DataExplorer', [
+            'subsetGroup' => $subsetGroup,
+            'subsets' => $subsets,
+            'oldTab' => $request->input('tab', 'state'),
+            'oldSubsetName' => $request->input('subset', null),
+        ]);
+    }
+}
