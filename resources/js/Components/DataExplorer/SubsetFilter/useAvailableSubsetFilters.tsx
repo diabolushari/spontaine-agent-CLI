f527580d5ed8b5c73615f44abe3a8e27628af574
thejzain <thejzain@gmail.com>
2025-07-16 21:48:59 +0530

    Filtering field includes Month

--- resources/js/Components/DataExplorer/SubsetFilter/useAvailableSubsetFilters.tsx
+++ resources/js/Components/DataExplorer/SubsetFilter/useAvailableSubsetFilters.tsx
@@ -29,6 +29,12 @@ export default function useAvailableSubsetFilters(
 
     dimensions.forEach((dimension) => {
       if (dimension.subset_column == 'month') {
+        fields.push({
+          fieldId: dimension.field_id,
+          fieldName: 'Month',
+          column: 'month',
+          type: 'dimension',
+        })
         return
       }
       if (dimension.subset_column === 'section_code') {
