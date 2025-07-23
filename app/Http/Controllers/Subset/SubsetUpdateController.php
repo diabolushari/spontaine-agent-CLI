71be3563965079cc0a69918128bd5db61e67e659
thejzain <thejzain@gmail.com>
2025-07-23 15:57:07 +0530

    Subset Edit working with the full options in create

--- app/Http/Controllers/Subset/SubsetUpdateController.php
+++ app/Http/Controllers/Subset/SubsetUpdateController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Subset;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Subset\SubsetFormRequest;
-use App\Libs\ExceptionMessage;
 use App\Models\Subset\SubsetDetail;
-use Exception;
+use App\Models\Subset\SubsetDetailDate;
+use App\Models\Subset\SubsetDetailDimension;
+use App\Models\Subset\SubsetDetailMeasure;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class SubsetUpdateController extends Controller implements HasMiddleware
 {
@@ -22,19 +25,64 @@ class SubsetUpdateController extends Controller implements HasMiddleware
         ];
     }
 
+    /**
+     * @throws Throwable
+     */
     public function __invoke(
         SubsetDetail $subsetDetail,
         SubsetFormRequest $request
     ): RedirectResponse {
-        try {
-            $subsetDetail->update($request->all());
-        } catch (Exception $exception) {
-            return redirect()
-                ->back()
-                ->with([
-                    'error' => ExceptionMessage::getMessage($exception),
-                ]);
-        }
+        DB::transaction(function () use ($subsetDetail, $request) {
+
+            $subsetDetail->update(
+                $request->except('dates', 'dimensions', 'measures')->toArray()
+            );
+
+            $subsetDetail->dates()->delete();
+            $subsetDetail->dimensions()->delete();
+            $subsetDetail->measures()->delete();
+
+            $user = request()->user()?->id;
+
+            $dates = array_map(function ($date) use ($subsetDetail, $user) {
+                return [
+                    'subset_detail_id' => $subsetDetail->id,
+                    'created_by' => $user,
+                    'updated_by' => $user,
+                    ...$date->toArray(),
+                ];
+            }, $request->dates ?? []);
+
+            $dimensions = array_map(function ($dimension) use ($subsetDetail, $user) {
+                return [
+                    'subset_detail_id' => $subsetDetail->id,
+                    'created_by' => $user,
+                    'updated_by' => $user,
+                    ...$dimension->toArray(),
+                ];
+            }, $request->dimensions ?? []);
+
+            $measures = array_map(function ($measure) use ($subsetDetail, $user) {
+                return [
+                    'subset_detail_id' => $subsetDetail->id,
+                    'created_by' => $user,
+                    'updated_by' => $user,
+                    ...$measure->toArray(),
+                ];
+            }, $request->measures ?? []);
+
+            if (! empty($dates)) {
+                SubsetDetailDate::insert($dates);
+            }
+            if (! empty($measures)) {
+                SubsetDetailMeasure::insert($measures);
+            }
+            if (! empty($dimensions)) {
+                foreach ($dimensions as $dimension) {
+                    SubsetDetailDimension::create($dimension);
+                }
+            }
+        });
 
         return redirect()
             ->route('subset.preview', $subsetDetail->id)
