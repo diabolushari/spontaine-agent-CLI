99f1f7334a5c0dfc1b6133fd5735a8528ce07828
xothejzain <thej@intuonfx.com>
2026-03-30 14:53:27 +0530

    fix: make heirarchy property nullable in SubsetFormRequest

--- app/Http/Requests/Subset/SubsetFormRequest.php
+++ app/Http/Requests/Subset/SubsetFormRequest.php
@@ -34,6 +34,6 @@ class SubsetFormRequest extends Data
         public ?array $dimensions,
         public ?array $measures,
         public ?array $texts,
-        public string $heirarchy,
+        public ?string $heirarchy,
     ) {}
 }
