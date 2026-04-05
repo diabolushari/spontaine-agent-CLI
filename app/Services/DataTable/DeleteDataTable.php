d3dc243fbb8b996476d9044401bcf2c10e3a90ca
Alan-A-F <alanfaisel@gmail.com>
2026-04-05 14:12:46 +0530

    delete fix

--- app/Services/DataTable/DeleteDataTable.php
+++ app/Services/DataTable/DeleteDataTable.php
@@ -13,74 +13,13 @@ readonly class DeleteDataTable
     {
         $dataDetail->load('dateFields', 'dimensionFields', 'relationFields');
 
-        // Store foreign key constraints before dropping them
-        $foreignKeys = $this->getForeignKeys($dataDetail);
-
-        // Drop indexes on dateFields and foreignIds on dimensionFields
-        Schema::table($dataDetail->table_name, function (Blueprint $table) use ($dataDetail) {
-            foreach ($dataDetail->dateFields as $dateField) {
-                $table->dropIndex(
-                    $dataDetail->table_name
-                    .'_'
-                    .$dateField->column
-                    .'_index'
-                );
-            }
-
-            // foreach ($dataDetail->dimensionFields as $dimensionField) {
-            //     $column = $dimensionField->column;
-
-            //     $fkName = 'fk_dt_'.$dataDetail->id.'_'.$column;
-
-            //     try {
-            //         $table->dropForeign($fkName);
-            //     } catch (\Throwable $e) {
-
-            //         $table->dropForeign($dataDetail->table_name.'_'.$dimensionField->column.'_foreign');
-            //         $table->dropIndex($dataDetail->table_name.'_'.$dimensionField->column.'_foreign');
-            //     }
-            // }
-            foreach ($dataDetail->dimensionFields as $dimensionField) {
-                $column = $dimensionField->column;
-
-                // Get actual FK name from DB
-                $fk = DB::selectOne('
-        SELECT CONSTRAINT_NAME
-        FROM information_schema.KEY_COLUMN_USAGE
-        WHERE TABLE_NAME = ?
-        AND COLUMN_NAME = ?
-        AND REFERENCED_TABLE_NAME IS NOT NULL
-    ', [
-                    $dataDetail->table_name,
-                    $column,
-                ]);
-                
-                if ($fk) {
-                    $table->dropForeign($fk->CONSTRAINT_NAME);
-                }
-            }
-        });
-
         $dataDetail->delete();
 
         if (Schema::hasTable($dataDetail->table_name)) {
             $archivedTableName = 'archived_'.time().'_'.$dataDetail->table_name;
             Schema::rename($dataDetail->table_name, $archivedTableName);
 
-            Schema::table($archivedTableName, function (Blueprint $table) use ($foreignKeys, $archivedTableName) {
-                foreach ($foreignKeys as $foreignKey) {
-                    $column = $foreignKey['column'];
-
-                    $indexName = 'idx_'.hash('crc32b', "{$archivedTableName}_{$column}");
-
-                    $foreignName = 'fk_'.hash('crc32b', "{$archivedTableName}_{$column}");
-
-                    $table->index($column, $indexName);
-                    $table->foreign($column, $foreignName)
-                        ->references('id')
-                        ->on($foreignKey['related_table']);
-                }
-            });
+            
         }
     }
 
